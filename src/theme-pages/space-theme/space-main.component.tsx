6e4a1a9b40820c9c961355ef778df363670a3d76
HorvDan <horv.dan94@gmail.com>
2024-10-14 17:14:15 +0200

    theme change for header sections

--- src/theme-pages/space-theme/space-main.component.tsx
+++ src/theme-pages/space-theme/space-main.component.tsx
@@ -1,10 +1,17 @@
+import OurServices from "../minimalist-theme/components/our-services/our-services.component";
+import styles from "./space.module.scss";
 const SpaceMainPage = () => {
   return (
-    <div>
-      <h1>SpaceMain</h1>
-      <h1></h1>
-      <h1></h1>
-    </div>
+    <>
+      <div className={styles.heroSection}>
+        <div className={styles.learnMoreGroup}>
+          <h1 className={styles.title}>We create websites.</h1>
+          <button className={styles.learnMoreButton}>Learn more</button>
+        </div>
+        <img className={styles.animatedLogo} src="/animated_logo.svg" alt="" />
+      </div>
+      <OurServices />
+    </>
   );
 };
 export default SpaceMainPage;
