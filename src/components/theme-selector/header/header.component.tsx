b05d525892b86d686d63af6c5157b03aa0207fb1
Bernath Mark Bence <naruto010190@gmail.com>
2024-10-24 12:09:56 +0200

    commit, so others can take over if they do not want to wait until I return

--- src/components/theme-selector/header/header.component.tsx
+++ src/components/theme-selector/header/header.component.tsx
@@ -1,20 +1,34 @@
 import styles from "./header.module.scss";
 import { useTheme } from "../../../theme-provider/theme-provider.context";
+import { useState } from "react";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const { theme } = useTheme();
 
   return (
     <div className={styles[theme]}>
       <header className={styles.wrapper}>
         <h1 className={styles.headerTitle}>C_C_M_</h1>
-        <button className={styles.hamburgerMenu}>
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className={styles.hamburgerMenu}
+        >
           <div className={styles.line}></div>
           <div className={styles.line}></div>
           <div className={styles.line}></div>
+
+          <div className={`${styles.menu} ${menuOpen ? styles.menuOpen : ""}`}>
+            <ul>
+              <li>Menu Item 1</li>
+              <li>Menu Item 2</li>
+              <li>Menu Item 3</li>
+            </ul>
+          </div>
         </button>
       </header>
     </div>
   );
 };
+
 export default Header;
