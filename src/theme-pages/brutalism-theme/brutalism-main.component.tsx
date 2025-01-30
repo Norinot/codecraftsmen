08935b8d4b1d5543d04aa22947068ed499591cb0
HorvDan <horv.dan94@gmail.com>
2025-01-30 10:05:15 +0100

    hotfix after merge

--- src/theme-pages/brutalism-theme/brutalism-main.component.tsx
+++ src/theme-pages/brutalism-theme/brutalism-main.component.tsx
@@ -1,29 +1,13 @@
 import OurServices from "./components/our-services/our-services.component";
 import OurTeam from "./components/our-team/our-team.components";
-import Typography from "../../components/theme-selector/typography/typography.component";
 import styles from "./brutalism.module.scss";
 
 const BrutalismMainPage = () => {
   return (
-    <>
-      <div className={styles.heroSection}>
-        <div className={styles.learnMoreGroup}>
-          <Typography
-            theme="Brutalist"
-            variant="body-large-bold"
-            className={styles.title}
-          >
-            We create websites.
-          </Typography>
-          <button className={styles.learnMoreButton}>Learn more</button>
-        </div>
-        <img className={styles.animatedLogo} src="/animated_logo.svg" alt="" />
-      </div>
-      <div className={styles.root}>
-        <OurServices />
-        <OurTeam />
-      </div>
-    </>
+    <div className={styles.root}>
+      <OurServices />
+      <OurTeam />
+    </div>
   );
 };
 export default BrutalismMainPage;
--- src/theme-pages/space-theme/space-main.component.tsx
+++ src/theme-pages/space-theme/space-main.component.tsx
@@ -1,17 +1,12 @@
-import OurServices from "../minimalist-theme/components/our-services/our-services.component";
+import OurServices from "../minimalist-theme/components/our-services/our-services.component"; // Change import location
+import OurTeam from "../minimalist-theme/components/our-team/our-team.component"; // Change import location
 import styles from "./space.module.scss";
 const SpaceMainPage = () => {
   return (
-    <>
-      <div className={styles.heroSection}>
-        <div className={styles.learnMoreGroup}>
-          <h1 className={styles.title}>We create websites.</h1>
-          <button className={styles.learnMoreButton}>Learn more</button>
-        </div>
-        <img className={styles.animatedLogo} src="/animated_logo.svg" alt="" />
-      </div>
+    <div className={styles.root}>
       <OurServices />
-    </>
+      <OurTeam />
+    </div>
   );
 };
 export default SpaceMainPage;
